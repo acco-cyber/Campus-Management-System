36bffb8026d85098c3672a4cf0a1d9f51956f038
KousHik Rudra <koushiktrade750@gmail.com>
2025-10-07 19:08:06 -0600

    Add files via upload

---
+++ Add_Data_to_Database.py
@@ -0,0 +1,28 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate(".\serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    "databaseURL":"https://project01-978ca-default-rtdb.firebaseio.com/"
+})
+
+
+ref=db.reference('Students')
+
+data = {
+    
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 0,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key,value in data.items():
+    ref.child(key).set(value)
